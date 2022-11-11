34b972f48bed7d476e1c9c016ebb62e7b10f3e43
森史憲 <mori@codingdesing.jp>
2022-11-11 12:47:09 +0900

    テーマカラーをtailwind.cssの設定ファイルに追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       fontFamily: {
         noto: ["'Noto Sans JP', sans-serif"]
       },
+      colors: {
+        'red-purple': '#DD1B57',
+      },
     },
   },
   plugins: [],
